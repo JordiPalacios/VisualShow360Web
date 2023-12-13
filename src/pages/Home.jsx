651323c38f22873e3b4b8608010a3b57476d42fd
unknown <jordipalacioseso@gmail.com>
2023-12-13 07:51:25 +0100

    Bug ID solved in ProductsCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,9 +55,9 @@ export const Home = () => {
         <section>
           <div className="service4Container">                      
             <h2>Servicio 4 - Productos Alquiler</h2>
-            <ProductsCard id='#letras-love' tittle='Letras Love' description='Letras Love Description' imgUrl='src\assets\icons\ig.svg'/>
-            <ProductsCard id='#neones' tittle='Neones' description='Neones Description' imgUrl='src\assets\icons\ig.svg'/>
-            <ProductsCard id='#kitAntiResaca' tittle='Kit AntiResaca' description='Kit AntiResaca Description' imgUrl='src\assets\icons\ig.svg'/>
+            <ProductsCard id='letras-love' tittle='Letras Love' description='Letras Love Description' imgUrl='src\assets\icons\ig.svg'/>
+            <ProductsCard id='neones' tittle='Neones' description='Neones Description' imgUrl='src\assets\icons\ig.svg'/>
+            <ProductsCard id='kitAntiResaca' tittle='Kit AntiResaca' description='Kit AntiResaca Description' imgUrl='src\assets\icons\ig.svg'/>
           </div>
         </section>
         <section id='caja-hora-loca'>

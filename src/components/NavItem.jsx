0bb5d30e5bfd678f9fe9bd96c690791ee0e99f59
unknown <jordipalacioseso@gmail.com>
2024-01-15 19:47:32 +0100

    Section 1 Modified buttons and style

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const NavItem = ({href, label, colorText}) => {
   return (
-    <li>
-        <a href={href} style={{ color: colorText }}>
+    <li style={{ listStyleType: 'none' }}>
+        <a href={href} style={{ color: colorText}}>
             {label}
         </a>
     </li>

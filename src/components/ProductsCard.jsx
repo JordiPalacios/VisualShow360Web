d351a8be0b1ffa1a67af0c21057ed4a7eaa9df83
unknown <jordipalacioseso@gmail.com>
2024-02-14 07:58:05 +0100

    Modifying Kit AntiResaca para verlo en fila

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -4,18 +4,54 @@ import { WhatsAppMessage } from './WhatsAppMessage'
 export const ProductsCard = ({ id, tittle, description, imgUrl, darkBg, colorText, msg }) => {
   const cardClassName = darkBg ? 'productCard darkBg' : 'productCard'
   const buttonClassName = darkBg ? 'buttonsContainer' : 'buttonsContainerEven'
-  const content = (
-    <div id={id} className={cardClassName}>        
-      <h3>{tittle}</h3>
-      <img src={imgUrl} alt={tittle} />
-      <p>{description}</p>
-      <div className={buttonClassName}>        
-        <button>
-          <WhatsAppMessage className="" label="Más Información" colorText={colorText} msg={msg} />
-        </button>
+  const displayElementRow = darkBg ? 'rowContainer' : ''
+
+  // ToDo: Acabar el diplay element row -> Quiero que sea en Row: Titulo y Descripcion a un lado + Foto al lado opuesto
+  // ! Debo Crear una nueva Distribucion del content si tengo un Input de Row ContentImg, asi puedo modificarlos como quiera
+
+  let content
+
+  if (displayElementRow !== 'rowContainer') {
+    content = (
+      <div id={id} className={cardClassName}>   
+        <h3>{tittle}</h3>
+        <img src={imgUrl} alt={tittle} />
+        <p>{description}</p>
+        <div className={buttonClassName}>        
+          <button>
+            <WhatsAppMessage className="" label="Más Información" colorText={colorText} msg={msg} />
+          </button>
+        </div>     
       </div>
-    </div>
-  )
+    )
+  } else {
+    content = (
+      <div id={id} className={cardClassName}>   
+        <img src={imgUrl} alt={tittle} />
+        <h3>{tittle}</h3>
+        <p>{description}</p>
+        <div className={buttonClassName}>        
+          <button>
+            <WhatsAppMessage className="" label="Más Información" colorText={colorText} msg={msg} />
+          </button>
+        </div>     
+      </div>
+    )
+  }
+
+  // const content = (
+  //   <div id={id} className={cardClassName}>   
+  //     <h3>{tittle}</h3>
+  //     <img src={imgUrl} alt={tittle} />
+  //     <p>{description}</p>
+  //     <div className={buttonClassName}>        
+  //       <button>
+  //         <WhatsAppMessage className="" label="Más Información" colorText={colorText} msg={msg} />
+  //       </button>
+  //   </div>     
+  //   </div>
+  // )
+
 
   return (
     <>

5976894578754c034812b6153a96b8c0139d04e2
Jordi <jordipalacioseso@gmail.com>
2024-02-19 12:56:04 +0100

    Updated html structure for normativity and other products

--- src/pages/AvisoLegal.jsx
+++ src/pages/AvisoLegal.jsx
@@ -4,89 +4,96 @@ import { NavbarMenu, NewPage } from '../components'
 export const AvisoLegal = () => {
 
     return (
-    <div className='generalContainer'>
-        <NavbarMenu />
-        <div className='normativityContainer'>
-            <h1>Adaptar a Visual Show</h1>
-            <h3>AVISO LEGAL</h3>
-            <h4>Datos identificativos:</h4>
-            <ul>
-                <li>Nombre comercial: Visual Show</li>
-                <li>Denominación Social: visualshow</li>
-                <li>Domicilio social: visualshow</li>
-                <li>Correo electrónico: info@visualshow.com</li>
-            </ul>
-            <p>
-                Fotobooth Atelier como responsables de esta web, asumen el compromiso de procesar la información de sus usuarios y clientes con plenas garantías y cumplir con los requisitos nacionales y europeos que regulan la recopilación y uso de los datos personales de sus usuarios. Esta web, por tanto, cumple rigurosamente con La Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal (LOPD), y con el Real Decreto 1720/2007, de 21 de diciembre (LOPD). Cumple también con el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de las personas físicas (RGPD), así como con la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y Comercio Electrónico (LSSICE ó LSSI).<br/>
-                La web de Fotobooth Atelier tiene el objetivo de informar sobre los servicios de la empresa y facilitar una vía de contacto con nosotras para una mayor información sobre nuestros servicios.
-            </p>
-            <h4>Condiciones de uso:</h4>
-            <p>
-                1.- Información general y de contacto.
+    <>
+        <header>
+            <NavbarMenu />
+        </header>
 
-                En cumplimiento de lo dispuesto en la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y del Comercio Electrónico, los responsables de la página web Party Crew Factory (https://partycrewfactory.com), en adelante Party Crew Factory, ponen en conocimiento de los usuarios de la presente página web los siguientes extremos, en relación con el prestador de servicios de la sociedad de la información:
+        <main>
+            <div className='normativityContainer'>
+                <div className='line'></div> 
+                <h1>Adaptar a Visual Show</h1>
+                <h3>AVISO LEGAL</h3>
+                <h4>Datos identificativos:</h4>
+                <ul>
+                    <li>Nombre comercial: Visual Show</li>
+                    <li>Denominación Social: visualshow</li>
+                    <li>Domicilio social: visualshow</li>
+                    <li>Correo electrónico: info@visualshow.com</li>
+                </ul>
+                <p>
+                    Fotobooth Atelier como responsables de esta web, asumen el compromiso de procesar la información de sus usuarios y clientes con plenas garantías y cumplir con los requisitos nacionales y europeos que regulan la recopilación y uso de los datos personales de sus usuarios. Esta web, por tanto, cumple rigurosamente con La Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal (LOPD), y con el Real Decreto 1720/2007, de 21 de diciembre (LOPD). Cumple también con el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de las personas físicas (RGPD), así como con la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y Comercio Electrónico (LSSICE ó LSSI).<br/>
+                    La web de Fotobooth Atelier tiene el objetivo de informar sobre los servicios de la empresa y facilitar una vía de contacto con nosotras para una mayor información sobre nuestros servicios.
+                </p>
+                <h4>Condiciones de uso:</h4>
+                <p>
+                    1.- Información general y de contacto.
 
-                Maria Sofia Sada, provista con NIE Y8831390W, con domicilio en Calle Riera Blanca 196, 6-1, 08902, Barcelona.
+                    En cumplimiento de lo dispuesto en la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y del Comercio Electrónico, los responsables de la página web Party Crew Factory (https://partycrewfactory.com), en adelante Party Crew Factory, ponen en conocimiento de los usuarios de la presente página web los siguientes extremos, en relación con el prestador de servicios de la sociedad de la información:
 
-                Adicionalmente, se informa a los usuarios que para el planteamiento o resolución de cualquier conflicto, incidencia o cuestión que pudiera surgir respecto del acceso a la presente página web, de su uso y / o de los contenidos puestos a disposición del público a través de la misma, quedan habilitadas las siguientes direcciones de contacto y comunicación directa y efectiva:
+                    Maria Sofia Sada, provista con NIE Y8831390W, con domicilio en Calle Riera Blanca 196, 6-1, 08902, Barcelona.
 
-                - Dirección postal de contacto: 
-                Maria Sofia Sada, provista con NIE Y8831390W, con domicilio en Calle Riera Blanca 196, 6-1, 08902, Barcelona.
+                    Adicionalmente, se informa a los usuarios que para el planteamiento o resolución de cualquier conflicto, incidencia o cuestión que pudiera surgir respecto del acceso a la presente página web, de su uso y / o de los contenidos puestos a disposición del público a través de la misma, quedan habilitadas las siguientes direcciones de contacto y comunicación directa y efectiva:
 
-                - Dirección electrónica de contacto: hola@partycrewfactory.com
+                    - Dirección postal de contacto: 
+                    Maria Sofia Sada, provista con NIE Y8831390W, con domicilio en Calle Riera Blanca 196, 6-1, 08902, Barcelona.
 
-                2.- Responsabilidad sobre los contenidos puestos a disposición del público a través de Party Crew Factory.
+                    - Dirección electrónica de contacto: hola@partycrewfactory.com
 
-                Los responsables de Party Crew Factory ponen en conocimiento de los usuarios que realizarán las actuaciones que resulten pertinentes a fin de garantizar la exactitud y precisión de la información puesta a disposición del público a través de la página web https:/partycrewfactory.com procediendo a la actualización periódica de la misma y a la corrección de eventuales errores con la mayor celeridad posible.
+                    2.- Responsabilidad sobre los contenidos puestos a disposición del público a través de Party Crew Factory.
 
-                No obstante, los responsables de Party Crew Factory no pueden garantizar la inexistencia de errores o la continua actualización de la información puesta a disposición del público a través de la mencionada página web.
+                    Los responsables de Party Crew Factory ponen en conocimiento de los usuarios que realizarán las actuaciones que resulten pertinentes a fin de garantizar la exactitud y precisión de la información puesta a disposición del público a través de la página web https:/partycrewfactory.com procediendo a la actualización periódica de la misma y a la corrección de eventuales errores con la mayor celeridad posible.
 
-                Igualmente, los responsables de Party Crew Factory no puede garantizar que el acceso  a la página web https://partycrewfactory.com no se vea temporalmente interrumpido por razones de índole técnica, ni que los contenidos a los que accedan los usuarios, o el software que sirve de base a los mismos, se encuentren permanentemente libres de errores.
+                    No obstante, los responsables de Party Crew Factory no pueden garantizar la inexistencia de errores o la continua actualización de la información puesta a disposición del público a través de la mencionada página web.
 
-                En consecuencia,  los responsables de Party Crew Factory no asumirán responsabilidad alguna, ya sea directa o indirecta, respecto de cualquier perjuicio causado a los usuarios que venga provocado o pudiera derivarse de interferencias, omisiones, interrupciones, averías telefónicas o desconexiones en el funcionamiento operativo del sistema electrónico, motivados por causas ajenas a Party Crew Factory , así como respecto de cualquier daño o perjuicio causado por la actuación de terceras personas mediante intromisiones ilegítimas en el sistema operativo, ya sea en forma de malware, virus, gusanos, troyanos, código malicioso, o cualesquiera otras figuras análogas.
+                    Igualmente, los responsables de Party Crew Factory no puede garantizar que el acceso  a la página web https://partycrewfactory.com no se vea temporalmente interrumpido por razones de índole técnica, ni que los contenidos a los que accedan los usuarios, o el software que sirve de base a los mismos, se encuentren permanentemente libres de errores.
 
-                Asimismo,  los responsables de Party Crew Factory quedan excluidos de cualquier responsabilidad que pudiera derivarse de retrasos  o bloqueos en el funcionamiento  operativo del sistema electrónico, que vengan causados por deficiencias o sobrecarga en las líneas telefónicas o de Internet.
+                    En consecuencia,  los responsables de Party Crew Factory no asumirán responsabilidad alguna, ya sea directa o indirecta, respecto de cualquier perjuicio causado a los usuarios que venga provocado o pudiera derivarse de interferencias, omisiones, interrupciones, averías telefónicas o desconexiones en el funcionamiento operativo del sistema electrónico, motivados por causas ajenas a Party Crew Factory , así como respecto de cualquier daño o perjuicio causado por la actuación de terceras personas mediante intromisiones ilegítimas en el sistema operativo, ya sea en forma de malware, virus, gusanos, troyanos, código malicioso, o cualesquiera otras figuras análogas.
 
-                En todo caso, el acceso a la página web https://partycrewfactory.com,  así como el uso que se efectúe de la información puesta a disposición del público a través de la misma, son realizados bajo la exclusiva y única responsabilidad del usuario.
+                    Asimismo,  los responsables de Party Crew Factory quedan excluidos de cualquier responsabilidad que pudiera derivarse de retrasos  o bloqueos en el funcionamiento  operativo del sistema electrónico, que vengan causados por deficiencias o sobrecarga en las líneas telefónicas o de Internet.
 
-                Los responsables de Party Crew Factory no asumirán responsabilidad alguna, ya sea directa o indirecta, respecto de ninguna consecuencia, daño o perjuicio, de cualquier índole, que eventualmente pudiera sufrir el usuario y que venga originado como consecuencia del acceso a la información puesta a disposición del público a través de la página web Party Crew Factory .
+                    En todo caso, el acceso a la página web https://partycrewfactory.com,  así como el uso que se efectúe de la información puesta a disposición del público a través de la misma, son realizados bajo la exclusiva y única responsabilidad del usuario.
 
-                Los responsables de Party Crew Factory se reservan el derecho a efectuar, en cualquier momento y sin previo aviso, modificaciones, supresiones o actualizaciones respecto de la información puesta a disposición del público a través de Party Crew Factory , en su configuración interna o en su presentación.
+                    Los responsables de Party Crew Factory no asumirán responsabilidad alguna, ya sea directa o indirecta, respecto de ninguna consecuencia, daño o perjuicio, de cualquier índole, que eventualmente pudiera sufrir el usuario y que venga originado como consecuencia del acceso a la información puesta a disposición del público a través de la página web Party Crew Factory .
 
-                Los responsables de Party Crew Factory, se reservan el derecho a suspender temporalmente y sin previo aviso, la accesibilidad a la página web referida, con motivo de operaciones de mantenimiento, reparación, actualización o mejora de las mismas.
+                    Los responsables de Party Crew Factory se reservan el derecho a efectuar, en cualquier momento y sin previo aviso, modificaciones, supresiones o actualizaciones respecto de la información puesta a disposición del público a través de Party Crew Factory , en su configuración interna o en su presentación.
 
-                Por último, los responsables de Party Crew Factory ponen en conocimiento de los usuarios que se encuentran puestos a disposición de los mismos, a través de la presente página web, los documentos denominados Condiciones Generales de Uso y Política de Privacidad, mediante cuya lectura los usuarios ostentan la posibilidad de ampliar su información en lo relativo a las condiciones de uso y navegación a través del presente sitio web, así como en lo relativo al compromiso de los responsables de Party Crew Factory con la privacidad y confidencialidad de los datos de carácter personal suministrados por los usuarios, respectivamente.
-            </p>
-        </div>
+                    Los responsables de Party Crew Factory, se reservan el derecho a suspender temporalmente y sin previo aviso, la accesibilidad a la página web referida, con motivo de operaciones de mantenimiento, reparación, actualización o mejora de las mismas.
+
+                    Por último, los responsables de Party Crew Factory ponen en conocimiento de los usuarios que se encuentran puestos a disposición de los mismos, a través de la presente página web, los documentos denominados Condiciones Generales de Uso y Política de Privacidad, mediante cuya lectura los usuarios ostentan la posibilidad de ampliar su información en lo relativo a las condiciones de uso y navegación a través del presente sitio web, así como en lo relativo al compromiso de los responsables de Party Crew Factory con la privacidad y confidencialidad de los datos de carácter personal suministrados por los usuarios, respectivamente.
+                </p>
+            </div>
+        </main>
+
         <footer>
             <div className="footerContainer">  
-            <h3>No hacemos eventos...</h3>      
-            <h4>¡ Creamos recuerdos !</h4>
-            <div className="line"></div>
-            <div className="rrssContainer">
-                <a href="https://www.instagram.com/visualshow360?igshid=YmMyMTA2M2Y=" target="_blank" rel="noopener noreferrer">
-                <img id="ig" src="src/assets/icons/ig.svg"/>
-                </a>            
-                <a href="https://www.tiktok.com/@visualshow360?_t=8jdUAUysy9p&_r=1" target="_blank" rel="noopener noreferrer">
-                <img id="ig" src="src/assets/icons/tiktok.svg"/>
-                </a>
-            </div>
-            <div className="normativeContainer">
-            <p>© 2024 VisualShow360 - Todos los derechos reservados</p>
-            <button>
-                <NewPage href="/aviso-legal" label="Aviso Legal" colorText='var(--primary-color-100)'/>
-            </button>
-            <button>
-                <NewPage href="/politica-privacidad" label="Politica de Privacidad" colorText='var(--primary-color-100)'/>
-            </button>
-            <button>
-                <NewPage href="/terminos-condiciones" label="Terminos y Condiciones" colorText='var(--primary-color-100)'/>
-            </button>
+                <h3>No hacemos eventos...</h3>      
+                <h4>¡ Creamos recuerdos !</h4>
+                <div className="line"></div>
+                <div className="rrssContainer">
+                    <a href="https://www.instagram.com/visualshow360?igshid=YmMyMTA2M2Y=" target="_blank" rel="noopener noreferrer">
+                    <img id="ig" src="src/assets/icons/ig.svg"/>
+                    </a>            
+                    <a href="https://www.tiktok.com/@visualshow360?_t=8jdUAUysy9p&_r=1" target="_blank" rel="noopener noreferrer">
+                    <img id="ig" src="src/assets/icons/tiktok.svg"/>
+                    </a>
+                </div>
+                <div className="normativeContainer">
+            <   p>© 2024 VisualShow360 - Todos los derechos reservados</p>
+                <button>
+                    <NewPage href="/aviso-legal" label="Aviso Legal" colorText='var(--primary-color-100)'/>
+                </button>
+                <button>
+                    <NewPage href="/politica-privacidad" label="Politica de Privacidad" colorText='var(--primary-color-100)'/>
+                </button>
+                <button>
+                    <NewPage href="/terminos-condiciones" label="Terminos y Condiciones" colorText='var(--primary-color-100)'/>
+                </button>
             </div>
             <img src="src/assets/img/Extra/generalitat.jpg" alt="generalitatImg" />
             <a href="https://www.instagram.com/jordi_code?igsh=MXRvbmtiYmhheHRiZw%3D%3D&utm_source=qr"> Creada y diseñada por Jordi Code</a>
             </div>
         </footer>
-    </div>
+    </>
 )
 }
--- src/pages/OtrosProductos.jsx
+++ src/pages/OtrosProductos.jsx
@@ -27,8 +27,12 @@ export const OtrosProductos = () => {
     }, [])
 
     return (
-        <div>
-            <NavbarMenu />
+        <>
+            <header>
+                <NavbarMenu />
+            </header>
+
+            <main>
             {/* RingRingExperience */}
             <section id='ring-ring-experience'>
                 <div className="service4Container">
@@ -49,68 +53,69 @@ export const OtrosProductos = () => {
             </section>
             {/* !Este section es de products */}
             <section id='products'>
-            <div className="productsContainer">
-                <div className="productsTittle">
-                <h2>Bodas y Eventos</h2>
-                </div>
-                <div className="generalProducts">
-                <ProductsCard
-                    id='letras-love'
-                    tittle='Letras Love'
-                    description=''
-                    msg="https://wa.me/34653781239?text=Quiero%20más%20información%20sobre%20las%20Letras%20Love!"
-                    imgUrl='src/assets/img/servicios/Neones.webp'
-                    difBg={false}
+                <div className="productsContainer">
+                    <div className="productsTittle">
+                    <h2>Bodas y Eventos</h2>
+                    </div>
+                    <div className="generalProducts">
+                        <ProductsCard
+                        id='letras-love'
+                        tittle='Letras Love'
+                        description=''
+                        msg="https://wa.me/34653781239?text=Quiero%20más%20información%20sobre%20las%20Letras%20Love!"
+                        imgUrl='src/assets/img/servicios/Neones.webp'
+                        difBg={false}
+                        colorText='var(--accent)'/>
+                        <ProductsCard
+                            id='neones'
+                            tittle='Neones'
+                            description=''
+                            msg="https://wa.me/34653781239?text=Quiero%20más%20información%20sobre%20los%20Neones!"
+                            imgUrl='src/assets/img/servicios/Neones.webp'
+                            difBg={false}
+                            colorText='var(--accent)'/>
+                    </div>
+                    <ProductsCard
+                    id='kitAntiResaca'
+                    tittle='Kit AntiResaca'
+                    description='Kit AntiResaca Description: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, eaque sunt autem iusto rem dolorum error harum ut quae sequi! Id, sint repellendus. In iste aperiam id aspernatur! Aspernatur, reprehenderit!'
+                    imgUrl='src/assets/img/servicios/kitAntiresacaFocus.webp'
+                    msg="https://wa.me/34653781239?text=Quiero%20más%20información%20sobre%20el%20Kit%20AntiResaca!"
+                    workingMode={workingMode}
+                    difBg={true}
                     colorText='var(--accent)'/>
-                <ProductsCard
-                    id='neones'
-                    tittle='Neones'
-                    description=''
-                    msg="https://wa.me/34653781239?text=Quiero%20más%20información%20sobre%20los%20Neones!"
-                    imgUrl='src/assets/img/servicios/Neones.webp'
-                    difBg={false}
-                    colorText='var(--accent)'/>
                 </div>
-                <ProductsCard
-                id='kitAntiResaca'
-                tittle='Kit AntiResaca'
-                description='Kit AntiResaca Description: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, eaque sunt autem iusto rem dolorum error harum ut quae sequi! Id, sint repellendus. In iste aperiam id aspernatur! Aspernatur, reprehenderit!'
-                imgUrl='src/assets/img/servicios/kitAntiresacaFocus.webp'
-                msg="https://wa.me/34653781239?text=Quiero%20más%20información%20sobre%20el%20Kit%20AntiResaca!"
-                workingMode={workingMode}
-                difBg={true}
-                colorText='var(--accent)'/>
+            </section>
+        </main>
+        <footer>
+            <div className="footerContainer">  
+            <h3>No hacemos eventos...</h3>      
+            <h4>¡ Creamos recuerdos !</h4>
+            <div className="line"></div>
+            <div className="rrssContainer">
+                <a href="https://www.instagram.com/visualshow360?igshid=YmMyMTA2M2Y=" target="_blank" rel="noopener noreferrer">
+                <img id="ig" src="src/assets/icons/ig.svg"/>
+                </a>            
+                <a href="https://www.tiktok.com/@visualshow360?_t=8jdUAUysy9p&_r=1" target="_blank" rel="noopener noreferrer">
+                <img id="ig" src="src/assets/icons/tiktok.svg"/>
+                </a>
             </div>
-        </section>
-            <footer>
-                <div className="footerContainer">  
-                <h3>No hacemos eventos...</h3>      
-                <h4>¡ Creamos recuerdos !</h4>
-                <div className="line"></div>
-                <div className="rrssContainer">
-                    <a href="https://www.instagram.com/visualshow360?igshid=YmMyMTA2M2Y=" target="_blank" rel="noopener noreferrer">
-                    <img id="ig" src="src/assets/icons/ig.svg"/>
-                    </a>            
-                    <a href="https://www.tiktok.com/@visualshow360?_t=8jdUAUysy9p&_r=1" target="_blank" rel="noopener noreferrer">
-                    <img id="ig" src="src/assets/icons/tiktok.svg"/>
-                    </a>
-                </div>
-                <div className="normativeContainer">
-                <p>© 2024 VisualShow360 - Todos los derechos reservados</p>
-                <button>
-                    <NewPage href="/aviso-legal" label="Aviso Legal" colorText='var(--secondary)'/>
-                </button>
-                <button>
-                    <NewPage href="/politica-privacidad" label="Politica de Privacidad" colorText='var(--secondary)'/>
-                </button>
-                <button>
-                    <NewPage href="/terminos-condiciones" label="Terminos y Condiciones" colorText='var(--secondary)'/>
-                </button>
-                </div>
-                <img src="src/assets/img/Extra/generalitat.jpg" alt="generalitatImg" />
-                <a href="https://www.instagram.com/jordi_code?igsh=MXRvbmtiYmhheHRiZw%3D%3D&utm_source=qr"> Creada y diseñada por Jordi Code</a>
-                </div>
-            </footer>
-        </div>
-    )
+            <div className="normativeContainer">
+            <p>© 2024 VisualShow360 - Todos los derechos reservados</p>
+            <button>
+                <NewPage href="/aviso-legal" label="Aviso Legal" colorText='var(--secondary)'/>
+            </button>
+            <button>
+                <NewPage href="/politica-privacidad" label="Politica de Privacidad" colorText='var(--secondary)'/>
+            </button>
+            <button>
+                <NewPage href="/terminos-condiciones" label="Terminos y Condiciones" colorText='var(--secondary)'/>
+            </button>
+            </div>
+            <img src="src/assets/img/Extra/generalitat.jpg" alt="generalitatImg" />
+            <a href="https://www.instagram.com/jordi_code?igsh=MXRvbmtiYmhheHRiZw%3D%3D&utm_source=qr"> Creada y diseñada por Jordi Code</a>
+            </div>
+        </footer>
+    </>
+)
 }

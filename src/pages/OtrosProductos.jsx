2936a47e617cf9b5c248c3a84a8a97cce2ed2eea
JordiCode <jordipalacioseso@gmail.com>
2024-03-12 15:53:24 +0100

    component section created for products2

--- src/pages/OtrosProductos.jsx
+++ src/pages/OtrosProductos.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { NavItem, NavbarMenu, WhatsAppMessage } from '../components'
+import {  NavbarMenu, WhatsAppMessage } from '../components'
 import { ContactUs, Footer, InfiniteLoopSection } from '../sections/Shared'
-import { Products2 } from '../sections/otherProductsPage/Products2'
-import { useWorkingMode } from '../assets/customhooks/useWorkingMode'
+import { Product1, Products2 } from '../sections/otherProductsPage'
 
 
 export const OtrosProductos = () => {
-    const workingMode = useWorkingMode()
+    
 
     return (
         <>           
@@ -14,62 +13,14 @@ export const OtrosProductos = () => {
                 <NavbarMenu />
             </header>
             <main>
-            {/* RingRingExperience */}
-            <section id='ring-ring-experience'>
-                {workingMode === 'PC' ? (
-                    <div className="service4Container">
-                        <div className="service4ContentContainer">
-                            <img src="src/assets/img/servicios/ringRing/audiolibro.webp" alt="InstruccionesRingRing" />
-                            <div className="service4DescriptionContainer">
-                                <h2>Ring Ring Experience</h2> 
-                                <h3> ¡Frase Guay &nbsp;<span>PC/Tablet!</span></h3> 
-                                <div className='line'></div>                      
-                                <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit qui nam dicta aspernatur non expedita iusto assumenda minus harum porro ullam similique voluptas neque autem modi ipsum, quisquam dolorum officiis?
-                                </p>
-                                <div className="buttonsContainer">
-                                    {/* <button>
-                                        <NewPage href="/eventos-reales" label="Eventos Reales" colorText='var(--accent-50)'/>
-                                    </button> */}
-                                    <button>
-                                        <NavItem href='#contacto' label='¡Agenda tu reunión!' colorText='var(--accent)' page='otrosProductos'/>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>                        
-                    </div>
-                ) : (
-                    <div className="service4Container">
-                        <h2>Ring Ring Experience</h2>
-                        <h3>Frase Guay Tablet</h3>
-                        <div className="service4ContentContainer">
-                            <img src="src/assets/img/servicios/ringRing/audiolibro.webp" alt="InstruccionesRingRing" />
-                            <div className="service4DescriptionContainer">
-                                    <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit qui nam dicta aspernatur non expedita iusto assumenda minus harum porro ullam similique voluptas neque autem modi ipsum, quisquam dolorum officiis?
-                                </p>
-                                <div className="buttonsContainer">
-                                    {/* <button>
-                                        <NewPage href="/eventos-reales" label="Eventos Reales" colorText='var(--accent-50)'/>
-                                    </button> */}
-                                    <button>
-                                        <NavItem href='#contacto' label='¡Agenda tu reunión!' colorText='var(--accent)' page='otrosProductos'/>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
-                
-            </section>
-            <InfiniteLoopSection />
-            {/* !Este section es de products */}
-            <Products2 />
-            <InfiniteLoopSection />
-            <ContactUs />
-            <WhatsAppMessage className="contactFixed" msg="https://wa.me/34653781239?text=Quiero%20más%20información%20sobre%20VisualShow!" />
-        </main>
-        <Footer />
+                <Product1 />
+                <InfiniteLoopSection />            
+                <Products2 />
+                <InfiniteLoopSection />
+                <ContactUs />
+                <WhatsAppMessage className="contactFixed" msg="https://wa.me/34653781239?text=Quiero%20más%20información%20sobre%20VisualShow!" />
+            </main>
+            <Footer />
     </>
 )
 }
--- src/sections/otherProductsPage/Product1.jsx
+++ src/sections/otherProductsPage/Product1.jsx
@@ -1,7 +1,50 @@
 import React from 'react'
+import { useWorkingMode } from '../../assets/customhooks/useWorkingMode'
+import { NavItem } from '../../components'
 
 export const Product1 = () => {
+    const workingMode = useWorkingMode()
+
 return (
-    <div>Product1</div>
+    <section id='ring-ring-experience'>
+        {workingMode === 'PC' ? (
+            <div className="service4Container">
+                <div className="service4ContentContainer">
+                    <img src="src/assets/img/servicios/ringRing/audiolibro.webp" alt="InstruccionesRingRing" />
+                    <div className="service4DescriptionContainer">
+                        <h2>Ring Ring Experience</h2> 
+                        <h3> ¡Frase Guay &nbsp;<span>PC/Tablet!</span></h3> 
+                        <div className='line'></div>                      
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit qui nam dicta aspernatur non expedita iusto assumenda minus harum porro ullam similique voluptas neque autem modi ipsum, quisquam dolorum officiis?
+                        </p>
+                        <div className="buttonsContainer">
+                            <button>
+                                <NavItem href='#contacto' label='¡Agenda tu reunión!' colorText='var(--accent)' page='otrosProductos'/>
+                            </button>
+                        </div>
+                    </div>
+                </div>                        
+            </div>
+        ) : (
+            <div className="service4Container">
+                <h2>Ring Ring Experience</h2>
+                <h3>Frase Guay Tablet</h3>
+                <div className="service4ContentContainer">
+                    <img src="src/assets/img/servicios/ringRing/audiolibro.webp" alt="InstruccionesRingRing" />
+                    <div className="service4DescriptionContainer">
+                            <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit qui nam dicta aspernatur non expedita iusto assumenda minus harum porro ullam similique voluptas neque autem modi ipsum, quisquam dolorum officiis?
+                        </p>
+                        <div className="buttonsContainer">
+                            <button>
+                                <NavItem href='#contacto' label='¡Agenda tu reunión!' colorText='var(--accent)' page='otrosProductos'/>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )}        
+    </section>
 )
 }

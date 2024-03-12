baf7c0db94d1b8cdbf5ee223f72235dcdc00401d
JordiCode <jordipalacioseso@gmail.com>
2024-03-12 12:12:00 +0100

    contact us component created

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -4,17 +4,19 @@ import { FormContact } from './FormContact'
 export const ContactUs = () => {
 
   const content = (
-    <div className="contactoContainer">
-      <h2>¡ SÍ Quiero !</h2>
-      <div className="elementsContainer">
-        <div className="formContainer">
-          <FormContact />
-        </div>
-        <div className="mediaContainer">
-          <img src="src/assets/img/Extra/logoVS360png.png" alt="logoVSpng" loading='lazy' />
+    <section id="contacto">
+      <div className="contactoContainer">
+        <h2>¡ SÍ Quiero !</h2>
+        <div className="elementsContainer">
+          <div className="formContainer">
+            <FormContact />
+          </div>
+          <div className="mediaContainer">
+            <img src="src/assets/img/Extra/logoVS360png.png" alt="logoVSpng" loading='lazy' />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
     
 return (

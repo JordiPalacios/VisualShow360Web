9065c83bf22b27ff444bb3c88397c9785961826a
JordiCode <jordipalacioseso@gmail.com>
2024-03-12 12:12:26 +0100

    companies component created

--- src/sections/homePage/Companies.jsx
+++ src/sections/homePage/Companies.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import CompaniesData from '../../assets/mocks/companiesData.json'
+import { InfiniteLoop } from '../../components'
 
 export const Companies = () => {
+    const companyData = CompaniesData
 return (
-    <div>Companies</div>
+    <section>
+        <div className="companiesContainer">
+            <h2>Empresas que han confiado en VisualShow</h2>
+            <div className="line"></div>
+            <div className="logoCompanies">
+            {companyData.map((data) => (
+                <InfiniteLoop key={data.companyName} urlImg={data.urlImg} nameImg={data.companyName} equal={false}/>
+            ))}
+            {companyData.map((data) => (
+                <InfiniteLoop key={data.companyName} urlImg={data.urlImg} nameImg={data.companyName} equal={false}/>
+            ))}
+            </div>
+        </div>
+        </section>
 )
 }

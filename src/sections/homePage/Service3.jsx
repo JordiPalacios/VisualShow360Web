0d053a4894cb1c663ac88facd32e70009764e2ff
JordiCode <jordipalacioseso@gmail.com>
2024-03-12 11:51:09 +0100

    All sections created and added in js barril

---
+++ src/sections/homePage/Service3.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Service3 = () => {
+return (
+    <div>Service3</div>
+)
+}
---
+++ src/sections/otherProductsPage/Product1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Product1 = () => {
+return (
+    <div>Product1</div>
+)
+}

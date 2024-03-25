c304732facca6d6ce9e877bb94b62bc972e56804
Jordi <jordipalacioseso@gmail.com>
2024-03-25 18:55:52 +0100

    src mocks reordered

--- src/sections/homePage/Companies.jsx
+++ src/sections/homePage/Companies.jsx
@@ -1,4 +1,4 @@
-import CompaniesData from '../../../public/mocks/companiesData.json'
+import CompaniesData from '../../assets/mocks/companiesData.json'
 import { InfiniteLoop } from '../../components'
 
 export const Companies = () => {

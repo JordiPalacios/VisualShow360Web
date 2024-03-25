4f507ed7903c2d6b37e817ef5591330c2e31aea3
Jordi <jordipalacioseso@gmail.com>
2024-03-25 13:55:23 +0100

    presentation page modified tittle

--- src/sections/homePage/Presentation.jsx
+++ src/sections/homePage/Presentation.jsx
@@ -14,8 +14,11 @@ return (
     <header>
         <NavbarMenu />
         <div className="presentationContainer">
-            <h1 className='h1-fade-in'> ¡ SE CASAN&nbsp;<span style={{ color: 'var(--accent)', fontWeight: '900'}}>190.000</span> PAREJAS AL AÑO !</h1>
-            <h2 className='h2-fade-in'>HAZ QUE TU BODA SEA ESPECIAL</h2>
+            <div className="tittles">                
+                <h1 className='h1-fade-in'> SE CASAN&nbsp;<span style={{ color: 'var(--accent)', fontWeight: '900'}}>190.000</span> PAREJAS AL AÑO</h1>
+                <h2 className='h2-fade-in'>HAZ QUE TU BODA SEA</h2>
+                <h2 className="h2-fade-in-accent">ESPECIAL</h2>
+            </div>
             <video className='videoMobile' autoPlay muted loop>
                 <source src="src/assets/video/videoMobileBW.mp4" type="video/mp4"/>
                 Este navegador no soporta vídeos.

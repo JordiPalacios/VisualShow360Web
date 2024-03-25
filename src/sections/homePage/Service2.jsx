c2c49fa758f9b6f0018290380e155ea8c8f4a8a7
Jordi <jordipalacioseso@gmail.com>
2024-03-25 13:57:01 +0100

    service 2 modified description and added new styles

--- src/sections/homePage/Service2.jsx
+++ src/sections/homePage/Service2.jsx
@@ -11,15 +11,31 @@ return (
             <img src="src/assets/gifs/GIF.gif" alt="Espejo magico" loading='lazy'/>           
             <div className="service2DescriptionContainer">  
                 <h1>Magic Mirror</h1>
-                <h2> ¡Frase Guay &nbsp;<span>PC/Tablet!</span></h2>    
-                <hr className='line'></hr>             
+                <h2> EL FOTOMATON QUE&nbsp;<span>NO SE ESPERAN</span></h2>    
+                <hr className='line'></hr>    
                 <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, quidem laudantium doloribus atque totam voluptate, nulla tenetur provident minima natus beatae non, reprehenderit pariatur praesentium! Dolore necessitatibus ab eaque cum!
-                </p>
+                    MAGIC MIRROR LED ES SÓLO  <span style={{ color: 'var(--accent)', background: 'none'}}>APTO</span> PARA <span style={{ color: 'var(--accent)', background: 'none'}}>PAREJAS</span> QUE QUIERAN <span style={{ color: 'var(--accent)', background: 'none'}}>DIFERENCIARSE</span> Y ALEJARSE DEL TÍPICO FOTOMATÓN DE HACE 10 AÑOS. <br />
+                    <br />
+                    AHORA, PREPÁRATE PORQUE TODOS <span style={{ color: 'var(--accent)', background: 'none'}}>TE VAN A PREGUNTAR</span> DE DÓNDE LO HAS SACADO. <br />
+                    <br />
+                    Asegúrate de que se lo cuenten a todos sus amigos, mira: <br />
+                    <span>&quot; Maria puso un fotomatón muy raro que fue la pxxx hostia, y vaya fiestón se montó &quot;</span>
+                    </p>
+                <div className="detailsService2"> 
+                    <h2>¿Qué incluye?</h2>                   
+                    <ul>
+                        <li>• Alfombra roja</li>
+                        <li>• Separadores de seguridad</li>
+                        <li>• Impresión al momento</li>
+                        <li>• Máxima calidad de imágen</li>
+                        <li>• La mejor mesa de ATREZZO que verás nunca</li>
+                    </ul>
+                    <span>CREAREMOS EL FIESTÓN </span>
+                </div>      
                 <div className="buttonsContainer">
-                <button>
-                    <NavItem href='#contacto' label='¡Agenda tu reunión!' colorText='var(--accent)' page='home'/>
-                </button>
+                    <button>
+                        <NavItem href='#contacto' label='¡Agenda tu reunión!' colorText='var(--accent)' page='home'/>
+                    </button>
                 </div>
             </div>
             </div>
@@ -30,17 +46,34 @@ return (
             <h1>Magic Mirror</h1>              
             <h2> ¡Frase Guay Para Tablet!</h2> 
             <div className="service2ContentContainer">              
-            <img src="src/assets/gifs/GIF.gif" alt="Espejo magico" loading='lazy'/>
-            <div className="service2DescriptionContainer">                
-                <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, quidem laudantium doloribus atque totam voluptate, nulla tenetur provident minima natus beatae non, reprehenderit pariatur praesentium! Dolore necessitatibus ab eaque cum!
-                </p>
-                <div className="buttonsContainer">
-                <button>
-                    <NavItem href='#contacto' label='¡Agenda tu reunión!' colorText='var(--accent)' page='home'/>
-                </button>
+                <img src="src/assets/gifs/GIF.gif" alt="Espejo magico" loading='lazy'/>
+                <div className="service2DescriptionContainer">                             
+                    <p>
+                        MAGIC MIRROR LED ES SÓLO  <span style={{ color: 'var(--accent)', background: 'none'}}>APTO</span> PARA <span style={{ color: 'var(--accent)', background: 'none'}}>PAREJAS</span> QUE QUIERAN <span style={{ color: 'var(--accent)', background: 'none'}}>DIFERENCIARSE</span> Y ALEJARSE DEL TÍPICO FOTOMATÓN DE HACE 10 AÑOS. <br />
+                        <br />
+                        AHORA, PREPÁRATE PORQUE TODOS <span style={{ color: 'var(--accent)', background: 'none'}}>TE VAN A PREGUNTAR</span> DE DÓNDE LO HAS SACADO. <br />
+                        <br />
+                        Asegúrate de que se lo cuenten a todos sus amigos, mira: <br />
+                        <br />
+                        <span>&quot; Maria puso un fotomatón muy raro que fue la pxxx hostia, y vaya fiestón se montó &quot;</span>
+                        </p>
+                    <div className="detailsService2"> 
+                        <h2>¿Qué incluye?</h2>                   
+                        <ul>
+                            <li>• Alfombra roja</li>
+                            <li>• Separadores de seguridad</li>
+                            <li>• Impresión al momento</li>
+                            <li>• Máxima calidad de imágen</li>
+                            <li>• La mejor mesa de ATREZZO que verás nunca</li>
+                        </ul>
+                        <span>CREAREMOS EL FIESTÓN </span>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button>
+                            <NavItem href='#contacto' label='¡Agenda tu reunión!' colorText='var(--accent)' page='home'/>
+                        </button>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
         )}

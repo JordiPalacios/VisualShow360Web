703ed08539daa1123f8b498ef6c3305c3be1f0d6
Jordi <jordipalacioseso@gmail.com>
2024-03-25 13:57:51 +0100

    logo changed and h2 too

--- src/sections/Shared/ContactUs.jsx
+++ src/sections/Shared/ContactUs.jsx
@@ -5,13 +5,14 @@ export const ContactUs = () => {
   const content = (
     <section id="contacto">
       <div className="contactoContainer">
-        <h2>¡ SÍ Quiero !</h2>
+        <h2>¡SÍ Quiero!</h2>
         <div className="elementsContainer">
           <div className="formContainer">
             <FormContact />
           </div>
           <div className="mediaContainer">
-            <img src="src/assets/img/Extra/logoVS360png.png" alt="logoVSpng" loading='lazy' />
+            {/* <img src="src/assets/img/Extra/logoVS360png.png" alt="logoVSpng" loading='lazy' /> */}
+            <img style={{ borderRadius: '50%', width: '300px', filter: 'drop-shadow(0 0 2px var(--accent-2)'}} src="src/assets/img/servicios/LogoVisualShow.webp" alt="logoVSpng" loading='lazy' />
           </div>
         </div>
       </div>

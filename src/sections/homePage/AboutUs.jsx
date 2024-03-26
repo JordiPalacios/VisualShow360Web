495271f905d20d462067fdc9d4976bd732bb1576
Jordi <jordipalacioseso@gmail.com>
2024-03-26 17:56:58 +0100

    in pc it's a row content and description colors changed

--- src/sections/homePage/AboutUs.jsx
+++ src/sections/homePage/AboutUs.jsx
@@ -3,26 +3,28 @@ return (
     <section id="nosotros">
         <div className="aboutUsContainer">
             <h2>¿Quienes Somos?</h2>
-            <img src="./img/reviews_equipo/navidadDavidIvet.webp" alt="VisualShow360Team" loading='lazy' />
-            <p>
-            ¡Somos Ivet & David! <br />
-            <br />
-            Hace años David me dijo unas palabras que nunca olvidaré: <br />
-            “Para que algo salga bien tiene que apasionarte” <br />
-            <br />
-            Somos y seremos los responsables de crear la mejor experiencia de fotomatón para tu boda. Alejándonos del típico servicio y llevándolo a otro nivel para que sea una experiencia más en tu evento y diferenciarte del resto de bodas. <br />
-            <br />
-            Y sobre todo, que os queden las mejores fotos de vuestra vida.   <br />
-            <br />
-            ¡Ah! Y se nos olvidaba, llevamos más de 5 años dedicándonos a eventos, y 2 años especializados en servicios para bodas. <br />
-            <br />
-            También somos pareja. Y somos el equipo perfecto. <br /> 
-            David tiene una idea, yo la pongo en marcha.  <br />
-            Y allí es donde le doy mi toque de personalidad, como si fuera yo la que me casara.   <br />
-            (Ya conoceréis los otros miembros del equipo, que sin ellos nada sería posible).  <br />
-            <br />
-            Y lo más importante, sabemos lo que buscais, por que también lo buscamos nosotros. <br />
-            </p>
+            <div className="contentContainer">                
+                <img src="./img/reviews_equipo/navidadDavidIvet.webp" alt="VisualShow360Team" loading='lazy' />
+                <p>
+                ¡Somos Ivet & David! <br />
+                <br />
+                Hace años David me dijo unas palabras que nunca olvidaré: <br />
+                <span>“Para que algo salga bien tiene que apasionarte”</span><br />
+                <br />
+                Somos y seremos los responsables de crear la <span>mejor experiencia</span> de fotomatón para tu boda. Alejándonos del típico servicio y llevándolo a otro nivel para que sea una experiencia más en tu evento y <span>diferenciarte</span> del resto de bodas. <br />
+                <br />
+                Y sobre todo, que os queden las mejores fotos de vuestra vida.   <br />
+                <br />
+                ¡Ah! Y se nos olvidaba, llevamos <span>más de 5 años</span> dedicándonos a eventos, y 2 años especializados en servicios para bodas. <br />
+                <br />
+                También somos pareja. Y somos el equipo perfecto. <br /> 
+                David tiene una idea, yo la pongo en marcha.  <br />
+                Y allí es donde le doy mi toque de personalidad, como si fuera yo la que me casara.   <br />
+                (Ya conoceréis los otros miembros del equipo, que sin ellos nada sería posible).  <br />
+                <br />
+                Y lo más importante, <span>sabemos lo que buscais</span>, por que también lo buscamos nosotros. <br />
+                </p>
+            </div>
         </div>
     </section>
 )

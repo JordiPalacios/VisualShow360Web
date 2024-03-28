eaea799ac1c58436635a68ef9989a99a93b3e991
Jordi <jordipalacioseso@gmail.com>
2024-03-28 09:07:12 +0100

    component for display media for each device

---
+++ src/components/MediaPresentation.jsx
@@ -0,0 +1,30 @@
+import { useCheckIOS } from "../assets/Customhooks/useCheckIOS"
+import { useWorkingMode } from "../assets/Customhooks/useWorkingMode"
+
+export const MediaRepresentation = () => {
+    const isIOS = true
+    // useCheckIOS()
+    const workingMode = useWorkingMode() 
+
+    const renderMedia = () => {
+        if (isIOS && workingMode === 'Mobile') {
+            return <img className='videoMobile' src="./img/servicios/fotomaton360/atrezzo_3.webp" alt="atrezzoVisualShow" />
+        } else if (isIOS && workingMode === 'Tablet') {
+            return <img className='videoTabletPC' src="./img/servicios/fotomaton360/atrezzo_3.webp" alt="atrezzoVisualShow" />
+        } else {
+            return (
+                <>
+                    <video className='videoMobile' controlsList='nodownload' playsInline loop muted autoPlay  >
+                        <source src="./video/videoMobileBW.mp4" type="video/mp4"/>
+                        Este navegador no soporta vídeos.
+                    </video>
+                        <video className='videoTabletPC'controlsList='nodownload' playsInline loop muted autoPlay >
+                        <source src="./video/weedingBW.mp4" type="video/mp4"/>
+                        Este navegador no soporta vídeos.
+                    </video>
+                </>
+            )
+        }
+    }
+    return <> {renderMedia()} </>
+}

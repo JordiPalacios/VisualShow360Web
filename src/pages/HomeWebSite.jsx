1d532878fa04aa7587549f6be9d05da38158d39f
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-01 13:30:59 +0200

    metaTag added and custom hook lazy load component to

--- src/pages/HomeWebSite.jsx
+++ src/pages/HomeWebSite.jsx
@@ -1,51 +1,48 @@
-import { LazyLoadComponent } from 'react-lazy-load-image-component';
-import { WhatsAppMessage } from '../components'
+import { MetaTags, WhatsAppMessage } from '../components'
 import { AboutUs, Companies, Presentation, Reviews, Service1, Service2, Service3 } from '../sections/homePage';
 import { ContactUs, Footer, InfiniteLoopSection } from '../sections/Shared';
-import { Helmet } from 'react-helmet';
+import { useLazyLoadComponent } from '../assets/Customhooks/useLazyLoadComponent';
 
 export const HomeWebsite = () => {
+  const LazyLoadService1 = useLazyLoadComponent(Service1)
+  const LazyLoadService2 = useLazyLoadComponent(Service2)
+  const LazyLoadService3 = useLazyLoadComponent(Service3)
+  const LazyLoadInfiniteLoop = useLazyLoadComponent(InfiniteLoopSection)
+  const LazyLoadReviews = useLazyLoadComponent(Reviews)
+  const LazyLoadCompanies = useLazyLoadComponent(Companies)
+  const LazyLoadFooter = useLazyLoadComponent(Footer)
+
 
   return (
     <>
-      <Helmet>
-        <title>VisualShow360 | Atrezzo & Fotomaton360</title>
-
-        <meta name='keywords' content='Fotomaton Barcelona '/>
-        {/*! Modificar decripcion */}
-        <meta name='description' content='El mejor fotomaton de barcelona con un atrezzo único. Haz de tu boda, evento, fiesta un momento único con nuestro espejo mágico.' />
-        <meta name='author' content='VisualShow360'/>
-        <meta name="rating" content="General" />
-        <meta property="og:title" content="VisualShow360" />
-        {/*! Modificar decripcion */}
-        <meta property="og:description" content="El mejor fotomaton de barcelona con un atrezzo único. Haz de tu boda, evento, fiesta un momento único con nuestro espejo mágico." />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://visualshow360.com/" />
-        {/*! Modificar foto del fotomaton */}
-        <meta property="og:image" content="./img/servicios/fotomaton360/fotomaton360_2.webp" />
-        <meta property="og:image:type" content="image/webp" />
-        <meta property="og:image:alt" content="Fotomaton 360 Barcelona" />
-
-        <link rel="canonical" href="https://visualshow360.com/" />             
-      </Helmet>
+      <MetaTags 
+        title={'VisualShow360 | Atrezzo & Fotomaton360'}
+        keywords={'Fotomaton Barcelona'}
+        desc={'El mejor fotomaton de barcelona con un atrezzo único. Haz de tu boda, evento, fiesta un momento único con nuestro espejo mágico.'}
+        author={'VisualShow360'}
+        rating={'General'}
+        ogTitle={'VisualShow360'}
+        ogDesc={'El mejor fotomaton de barcelona con un atrezzo único. Haz de tu boda, evento, fiesta un momento único con nuestro espejo mágico.'}
+        ogType={'website'}
+        ogUrl={'https://visualshow360.com/'}
+        ogImg={'./img/servicios/fotomaton360/fotomaton360_2.webp'}
+        OgImgType={'image/webp'}
+        ogImgAlt={'Fotomaton 360 Barcelona'}
+      />
       <Presentation />
       <main>
-        <LazyLoadComponent>
-          <Service1 />            
-          <InfiniteLoopSection />        
-          <Service2 />
-          <Service3 /> 
-          <InfiniteLoopSection />
-          <Reviews />         
-          <Companies />
-        </LazyLoadComponent>              
+        <LazyLoadService1 />            
+        <LazyLoadInfiniteLoop />        
+        <LazyLoadService2 />
+        <LazyLoadService3 /> 
+        <LazyLoadInfiniteLoop />
+        <LazyLoadReviews />         
+        <LazyLoadCompanies />              
         <AboutUs />
         <ContactUs />
         <WhatsAppMessage className="contactFixed" msg="https://wa.me/34653781239?text=¡Quiero%20más%20información%20sobre%20VisualShow!" />
       </main>
-      <LazyLoadComponent>        
-        <Footer />
-      </LazyLoadComponent>
+      <LazyLoadFooter />
     </>
   )
 }

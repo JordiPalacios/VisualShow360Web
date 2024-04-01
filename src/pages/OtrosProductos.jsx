68a29a062ab81dd3ab16b82ff251ec0e4ee8741b
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-01 13:35:19 +0200

    metTags added

--- src/pages/OtrosProductos.jsx
+++ src/pages/OtrosProductos.jsx
@@ -1,34 +1,25 @@
-import {  NavbarMenu, WhatsAppMessage } from '../components'
+import {  MetaTags, NavbarMenu, WhatsAppMessage } from '../components'
 import { ContactUs, Footer, InfiniteLoopSection } from '../sections/Shared'
 import { Product1, Products2 } from '../sections/otherProductsPage'
-import { Helmet } from 'react-helmet'
-
 
 export const OtrosProductos = () => {
     
-
     return (
-        <>        
-            <Helmet>
-                <title>VisualShow360 | RingRing, Neones & Letras Love</title>
-
-                <meta name='keywords' content='RingRing, Neones, Letras Love, Barcelona, Atrezzo, Bodas, Eventos, Catalunya' />
-                {/*! Modificar decripcion */}
-                <meta name='description' content='El mejor audiolibro de barcelona para bodas, fiestas, eventos. Especialistas en crear momentos únicos para tus momentos más especiales.' />
-                <meta name='author' content='VisualShow360'/>
-                <meta name="rating" content="General" />
-                <meta property="og:title" content="VisualShow360" />
-                {/*! Modificar decripcion */}
-                <meta property="og:description" content="El mejor audiolibro de barcelona para bodas, fiestas, eventos. Especialistas en crear momentos únicos para tus momentos más especiales." />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://visualshow360.com/" />
-                {/*! Modificar foto del fotomaton */}
-                <meta property="og:image" content="./img/servicios/ringRing/audiolibro.webp" />
-                <meta property="og:image:type" content="image/webp" />
-                <meta property="og:image:alt" content="Audio Libro Barcelona" />
-
-                <link rel="canonical" href="https://visualshow360.com/" />             
-            </Helmet>
+        <>      
+            <MetaTags 
+                title={'VisualShow360 | RingRing, Neones & Letras Love'}
+                keywords={'RingRing, Neones, Letras Love, Barcelona, Atrezzo, Bodas, Eventos, Catalunya'}
+                desc={'El mejor audiolibro de barcelona para bodas, fiestas, eventos. Especialistas en crear momentos únicos para tus momentos más especiales.'}
+                author={'VisualShow360'}
+                rating={'General'}
+                ogTitle={'VisualShow360'}
+                ogDesc={'El mejor audiolibro de barcelona para bodas, fiestas, eventos. Especialistas en crear momentos únicos para tus momentos más especiales.'}
+                ogType={'website'}
+                ogUrl={'https://visualshow360.com/otros-productos'}
+                ogImg={'./img/servicios/ringRing/audiolibro.webp'}
+                OgImgType={'image/webp'}
+                ogImgAlt={'Audio Libro Barcelona'}
+            />  
             <header>
                 <NavbarMenu />
             </header>

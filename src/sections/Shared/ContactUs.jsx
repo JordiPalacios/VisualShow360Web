54999d42e0cb0ec633fc58e36714b3ea4f0c48bd
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-01 12:26:10 +0200

    lazy component deleted

--- src/sections/Shared/ContactUs.jsx
+++ src/sections/Shared/ContactUs.jsx
@@ -5,17 +5,17 @@ export const ContactUs = () => {
 
   const content = (
     <section id="contacto">
-      <div className="contactoContainer">
-        <h2>¡SÍ Quiero!</h2>
-        <div className="elementsContainer">
-          <div className="formContainer">
-            <FormContact />
-          </div>
-          <div className="mediaContainer">
-            <LazyLoadImage style={{ borderRadius: '50%', width: '300px', filter: 'drop-shadow(0 0 2px var(--accent-2)'}} src="./img/servicios/LogoVisualShow.webp" alt="logoVSwebp" />
+        <div className="contactoContainer">
+          <h2>¡SÍ Quiero!</h2>
+          <div className="elementsContainer">
+            <div className="formContainer">       
+              <FormContact />
+            </div>
+            <div className="mediaContainer">
+              <LazyLoadImage style={{ borderRadius: '50%', width: '300px', filter: 'drop-shadow(0 0 2px var(--accent-2)'}} src="./img/servicios/LogoVisualShow.webp" alt="logoVSwebp" />
+            </div>
           </div>
         </div>
-      </div>
     </section>
   )
     

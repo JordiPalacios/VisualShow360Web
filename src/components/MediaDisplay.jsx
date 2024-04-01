4a031657c4be8d42707af243338aaa5c369fd440
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-01 18:53:37 +0200

    code simplified

--- src/components/MediaDisplay.jsx
+++ src/components/MediaDisplay.jsx
@@ -6,36 +6,29 @@ export const MediaDisplay = () => {
     const workingMode = useWorkingMode('Mobile') 
 
     const renderMedia = () => {
-        if (workingMode === 'Mobile') {
-            if (isIOS) {                
-                return <img className='imgMobile' src="./img/servicios/fotomaton360/atrezzo_3.webp" alt="atrezzoVisualShow" />
-            } else {
-                return (
-                    <video className='videoMobile' controlsList='nodownload' playsInline loop muted autoPlay  >
-                        <source src="./video/videoMobileBW.mp4" type="video/mp4"/>
-                        Este navegador no soporta vídeos.
-                    </video>                    
-                )
-            }
-        } else if (workingMode === 'Tablet') {
-            if (isIOS) {                
-                return <img className='imgTablet' src="./img/servicios/fotomaton360/atrezzo_3.webp" alt="atrezzoVisualShow" />
-            } else {
-                return (                    
-                    <video className='videoMobile' controlsList='nodownload' playsInline loop muted autoPlay  >
-                        <source src="./video/videoMobileBW.mp4" type="video/mp4"/>
-                        Este navegador no soporta vídeos.
-                    </video>                    
-                )
-            }
-        } else {
-            return (                             
-                <video className='videoTabletPC'controlsList='nodownload' playsInline loop muted autoPlay >
-                    <source src="./video/weedingBW.mp4" type="video/mp4"/>
+        if (workingMode === 'PC') {
+            return (
+                <video className='videoTabletPC' controlsList='nodownload' playsInline loop muted autoPlay > 
+                    <source src="./video/VisualShow.mp4" type="video/mp4"></source>
                     Este navegador no soporta vídeos.
-                </video>                
+                </video>
             )
         }
+
+        const className = `img${workingMode}`
+        const src = "./img/servicios/fotomaton360/atrezzo_3.webp"
+        const alt = "atrezzoVisualShow360"
+        
+        if (isIOS) {
+            return <img className={className} src={src} alt={alt} />
+        }
+
+        return (
+            <video className="videoMobile" controlsList='nodownload' playsInline loop muted autoPlay > 
+                <source src="./video/videoMobileBW.mp4" type="video/mp4" />
+                Este navegador no soporta vídeos.
+            </video>
+        )
     }
 
     return <> {renderMedia()} </>

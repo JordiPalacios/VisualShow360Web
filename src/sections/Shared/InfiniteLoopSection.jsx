e8775bfc2c6b191bb5ad5b66b1d25c80582343a8
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-01 12:25:16 +0200

    lazy load component moved only for infinite loop

--- src/sections/Shared/InfiniteLoopSection.jsx
+++ src/sections/Shared/InfiniteLoopSection.jsx
@@ -1,11 +1,14 @@
+import { LazyLoadComponent } from 'react-lazy-load-image-component'
 import { InfiniteLoop } from '../../components/'
 
 export const InfiniteLoopSection = () => {
 return (
     <section>
         <div className="logo">
-            <InfiniteLoop urlImg="./img/Extra/show.webp" nameImg="VisualShow" equal={true}/>
-            <InfiniteLoop urlImg="./img/Extra/show.webp" nameImg="VisualShow" equal={true}/>
+            <LazyLoadComponent>                
+                <InfiniteLoop urlImg="./img/Extra/show.webp" nameImg="VisualShow" equal={true}/>
+                <InfiniteLoop urlImg="./img/Extra/show.webp" nameImg="VisualShow" equal={true}/>
+            </LazyLoadComponent>
         </div>
     </section>
 )

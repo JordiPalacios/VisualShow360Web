cb7a76a948731a4716b9454cf9bdb427e103b02c
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-01 14:55:07 +0200

    use lazy load image if we work with pc

--- src/components/InfiniteLoop.jsx
+++ src/components/InfiniteLoop.jsx
@@ -1,17 +1,23 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import CompaniesData from '../assets/mocks/companiesData.json'
 import PropTypes from 'prop-types'
+import { useWorkingMode } from '../assets/Customhooks/useWorkingMode'
 
 export const InfiniteLoop = ({ urlImg, nameImg, equal }) => {
     const companyData = CompaniesData
     const numImages = Array(32).fill(null)
+    const workingMode = useWorkingMode('Mobile')
 
     let content = equal 
         ? numImages.map((_, index) => (
-            <LazyLoadImage key={index} src={urlImg} alt={nameImg} />
+            (workingMode === 'Mobile') 
+            ? <img key={index} src={urlImg} alt={nameImg} />
+            : <LazyLoadImage key={index} src={urlImg} alt={nameImg} />
         ))
         : companyData.map((data, index) => (
-            <LazyLoadImage key={index} src={data.urlImg} alt={data.companyName} />
+            (workingMode === 'Mobile')
+            ? <img key={index} src={data.urlImg} alt={data.companyName} />
+            : <LazyLoadImage key={index} src={data.urlImg} alt={data.companyName} />
         )) 
     
 return (

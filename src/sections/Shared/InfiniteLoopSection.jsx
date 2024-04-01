2409d4bea4ab99a5d751e740a47e3832c6b58721
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-01 20:27:32 +0200

    lazy infinite loop added

--- src/sections/Shared/InfiniteLoopSection.jsx
+++ src/sections/Shared/InfiniteLoopSection.jsx
@@ -1,6 +1,7 @@
+import { useEffect, useRef, useState } from 'react'
 import { InfiniteLoop } from '../../components/'
 
-export const InfiniteLoopSection = () => {
+const InfiniteLoopSection = () => {
 return (
     <section>
         <div className="logo">        
@@ -15,4 +16,33 @@ return (
         </div>
     </section>
 )
+}
+
+export const LazyInfiniteLoopSection = () => {
+    const [show, setShow] = useState(false)
+    const elementRef = useRef()
+
+    useEffect( function() {
+        const onChange = (entries, observer) => {
+            const element = entries[0]
+            if (element.isIntersecting) {
+                setShow(true)
+                observer.disconnect()
+            }
+        }
+
+        const observer = new IntersectionObserver(onChange, {
+            rootMargin: '100px'
+        })
+
+        observer.observe(elementRef.current)
+
+        return () => observer.disconnect()
+    })
+
+    return (
+        <section ref={elementRef}>
+            {show ? <InfiniteLoopSection /> : null}
+        </section>
+    )
 }

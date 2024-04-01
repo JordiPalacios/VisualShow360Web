81a246ca483fc7644a438ae956aa896597933c29
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-01 20:06:19 +0200

    fixed bug videos with media

--- src/components/MediaDisplay.jsx
+++ src/components/MediaDisplay.jsx
@@ -3,34 +3,32 @@ import { useWorkingMode } from "../assets/Customhooks/useWorkingMode"
 
 export const MediaDisplay = () => {
     const isIOS = useCheckIOS(false)
-    const workingMode = useWorkingMode('Mobile') 
+    const workingMode = useWorkingMode() 
+    const className = `img${workingMode}`
+    const src = "./img/servicios/fotomaton360/atrezzo_3.webp"
+    const alt = "atrezzoVisualShow360"
 
-    const renderMedia = () => {
-        if (workingMode === 'PC') {
-            return (
-                <video className='videoTabletPC' controlsList='nodownload' playsInline loop muted autoPlay > 
-                    <source src="./video/VisualShow.mp4" type="video/mp4"></source>
-                    Este navegador no soporta vídeos.
-                </video>
-            )
-        }
+    console.log("workingMode: ", workingMode)
 
-        const className = `img${workingMode}`
-        const src = "./img/servicios/fotomaton360/atrezzo_3.webp"
-        const alt = "atrezzoVisualShow360"
-        
-        if (isIOS) {
-            return <img className={className} src={src} alt={alt} />
-        }
-
-        return (
-            <video className="videoMobile" controlsList='nodownload' playsInline loop muted autoPlay > 
-                <source src="./video/videoMobileBW.mp4" type="video/mp4" />
+    let media
+    
+    if ( isIOS && workingMode !== 'PC' ) {
+        media = <img className={className} src={src} alt={alt} />
+    } else if (workingMode === 'Mobile') {
+        media = (
+            <video className='videoMobile' controlsList='nodownload' playsInline loop muted autoPlay  >
+                <source src="./video/videoMobileBW.mp4" type="video/mp4"/>
+                Este navegador no soporta vídeos.
+            </video>
+        )
+    } else if (workingMode === 'PC' || (workingMode === 'Tablet' && !isIOS)) {
+        media = (
+            <video className='videoTabletPC'controlsList='nodownload' playsInline loop muted autoPlay >
+                <source src="./video/weedingBW.mp4" type="video/mp4"/>
                 Este navegador no soporta vídeos.
             </video>
         )
     }
 
-    return <> {renderMedia()} </>
-
+    return <> {media} </>
 }

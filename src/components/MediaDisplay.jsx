3ba607e6084d35b7d4e53439e748c04d9bfa614e
JordiPalacios <jordipalacioseso@gmail.com>
2024-10-29 20:01:56 +0100

    header img changed

--- src/components/MediaDisplay.jsx
+++ src/components/MediaDisplay.jsx
@@ -5,7 +5,7 @@ export const MediaDisplay = () => {
     const isIOS = useCheckIOS(false)
     const workingMode = useWorkingMode() 
     const className = `img${workingMode}`
-    const src = "./img/servicios/fotomaton360/atrezzo_3.webp"
+    const src = "./img/servicios/whiteMagicMirror/whiteMagicMirror_11.webp"
     const alt = "atrezzoVisualShow360"
 
     let media

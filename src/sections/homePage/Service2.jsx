5f3ed8482935bb53294969fcdc0cfd5482bd92ef
JordiPalacios <jordipalacioseso@gmail.com>
2024-10-29 19:28:48 +0100

    slider added for magic mirror led

--- src/sections/homePage/Service2.jsx
+++ src/sections/homePage/Service2.jsx
@@ -1,16 +1,57 @@
-import { NavItem } from '../../components'
+import { ImgSlider, NavItem } from '../../components'
 import { useWorkingMode } from '../../assets/Customhooks/useWorkingMode'
-import { useContext, useEffect, useRef } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { LoadContext } from '../../context/LoadContext'
+import magicMirrorLedData from '../../assets/mocks/magicMirrorLedData.json'
+import Slider from 'react-slick';
 
 const Service2 = () => {
     const workingMode = useWorkingMode()
+
+    const fotomatonData = magicMirrorLedData
+
+    useEffect (() => {
+        const handleResize = () => {
+        const newSetttings = window.innerWidth < 1024
+        ? {...sliderSettings, slidesToShow: 1}
+        : {...sliderSettings, slidesToShow: 1}
+
+        setSliderSettings(newSetttings)
+        }
+    
+        window.addEventListener('resize', handleResize)
+
+        handleResize()
+        
+        return () => {
+        window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+    const [sliderSettings, setSliderSettings] = useState({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    })
+
 return (
     <section id='magic-mirror'>
         {workingMode === 'PC' ? (
         <div className='service2Container'>
             <div className="service2ContentContainer">
-            <img src="./gifs/GIF.gif" alt="Espejo magico" />           
+            <div className="fotomatonImgService2">            
+                <Slider {...sliderSettings}>
+                    {fotomatonData.map((data) => (
+                        <ImgSlider 
+                        key={data.id} 
+                        urlImg={data.urlImg} 
+                        imgName={data.id} 
+                        styleType='services'/>
+                        ))}
+                </Slider>
+            </div>
             <div className="service2DescriptionContainer">  
                 <h1>Magic Mirror LED</h1>
                 <h2> EL FOTOMATÓN QUE&nbsp;<span>NO SE ESPERAN</span></h2>    
@@ -48,7 +89,17 @@ return (
             <h1>Magic Mirror LED</h1>              
             <h2>EL FOTOMATÓN QUE&nbsp;<span>NO SE ESPERAN</span></h2> 
             <div className="service2ContentContainer">              
-                <img src="./gifs/GIF.gif" alt="Espejo magico" />
+                <div className="fotomatonImgService2">            
+                    <Slider {...sliderSettings}>
+                        {fotomatonData.map((data) => (
+                            <ImgSlider 
+                            key={data.id} 
+                            urlImg={data.urlImg} 
+                            imgName={data.id} 
+                            styleType='services'/>
+                            ))}
+                    </Slider>
+                </div>
                 <div className="service2DescriptionContainer">                             
                     <p>
                         MAGIC MIRROR LED ES SÓLO  <span style={{ color: 'var(--accent)', background: 'none'}}>APTO</span> PARA <span style={{ color: 'var(--accent)', background: 'none'}}>PAREJAS</span> QUE QUIERAN <span style={{ color: 'var(--accent)', background: 'none'}}>DIFERENCIARSE</span> Y ALEJARSE DEL TÍPICO FOTOMATÓN DE HACE 10 AÑOS. <br />

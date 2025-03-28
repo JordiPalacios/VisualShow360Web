0217df5f7e5636db5f0965c9a7460675fc63e751
JordiPalacios <jordipalacioseso@gmail.com>
2025-03-28 16:25:51 +0100

    componente para vídeo creado

---
+++ src/components/Video.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+import { useRef } from 'react'
+
+export const Video = ({ id = null, className, src, controls = false, muted = false, autoPlay = false}) => {
+    const videoRef = useRef(null)
+    const [isPlaying, setIsPlaying] = useState(autoPlay)
+
+    const handleVideoClick = () => {
+        const video = videoRef.current
+        if (!video) return
+
+        if (isPlaying) {
+            video.pause()
+            setIsPlaying(false)
+        } else {
+            video.play()
+            setIsPlaying(true)
+        }
+    }
+
+return (
+    <video 
+        id={id}
+        ref={videoRef}
+        className={className}
+        controls = {controls}
+        muted = {muted}
+        autoPlay = {autoPlay}
+        onClick= {handleVideoClick}
+        src={src}
+    />
+)
+}

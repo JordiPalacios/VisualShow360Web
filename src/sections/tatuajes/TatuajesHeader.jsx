65d4478b7062bf03295eb8b61cf148d4c36803bd
JordiPalacios <jordipalacioseso@gmail.com>
2025-04-09 08:15:33 +0200

    presentacion añadida con el slider y el texto descriptivo junto a sus dos titulos

---
+++ src/sections/tatuajes/TatuajesHeader.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import TatuajesData from '../../assets/mocks/tatuajesData.json'
+import { ImgSlider } from '../../components'
+import Slider from 'react-slick'
+
+export const TatuajesHeader = () => {
+    const tatuajesData = TatuajesData
+    useEffect (() => {
+            const handleResize = () => {
+            const newSetttings = window.innerWidth < 1024
+            ? {...sliderSettings, slidesToShow: 1}
+            : {...sliderSettings, slidesToShow: 3}
+    
+            setSliderSettings(newSetttings)
+            }
+        
+            window.addEventListener('resize', handleResize)
+    
+            handleResize()
+            
+            return () => {
+            window.removeEventListener('resize', handleResize)
+            }
+        }, [])
+
+        const [sliderSettings, setSliderSettings] = useState({
+                dots: false,
+                infinite: true,
+                speed: 500,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            })
+
+    return (
+    <div className='tatuajesHeader'>
+        <h1>Tatuajes</h1>
+        <div className="fotomatonImgTatuajes">            
+            <Slider {...sliderSettings}>
+                {tatuajesData.map((data) => (
+                    <ImgSlider 
+                    key={data.id} 
+                    urlImg={data.url} 
+                    imgName={data.name} 
+                    styleType='services'/>
+                ))}
+            </Slider>
+        </div> 
+        <h2> 
+            TUS INVITADOS VAN A FLIPAR CUANDO VEAN ESTO..
+        </h2>
+        <p>
+            ¿Nos jugamos algo?
+            <br />
+            <br />
+            Seguro que no se esperan ver ese tatuaje
+            que ya tienes en mente
+        </p>
+    </div>
+    )
+}

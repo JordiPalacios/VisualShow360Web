0f7971ecb61020c27d4fe446edb995e56615c422
JordiPalacios <jordipalacioseso@gmail.com>
2025-04-09 08:14:37 +0200

    texto explicativo para la explicacion 02 añadido

--- src/sections/tatuajes/TatuajesExplicacion02.jsx
+++ src/sections/tatuajes/TatuajesExplicacion02.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
-export const TatuajeExplicacion02 = () => {
+export const TatuajesExplicacion02 = () => {
   return (
-    <div>TatuajeExplicacion02</div>
+    <>
+        <img src="./img/reviews_equipo/navidadDavidIvet.webp" alt="VisualShow360Team" />
+        <p>
+          Un logo, tu perro, tu primo del pueblo, tu abuela con gafas de sol..
+          <br />
+          <br />
+          <b>Cualquier cosa</b> que quieras que tus invitados recuerden y lleven en su piel durante
+          días.
+          <br />
+          <br />
+          Y por <b>menos de lo que cuesta un café.</b>
+          <br />
+          <br />
+          0,50 centimos, ridículo
+          <br />
+          <br />
+          Lo dicho, si quieres quedarte con todos tus invitados y en la hora de la fiesta sacar
+          unos tatuajes personalizados, ya no tienes excusa.
+          <br />
+          <br />
+          <b>Envíanos tu diseño y vemos si quedan unidades disponibles:</b>
+        </p>
+        <button className='buttonsContainer'>
+          <a href="https://wa.me/message/2RM67GODIWVUE1" target="_blank" rel="noopener noreferrer">VER DISPONIBILIDAD</a>
+        </button>
+    </>
   )
 }

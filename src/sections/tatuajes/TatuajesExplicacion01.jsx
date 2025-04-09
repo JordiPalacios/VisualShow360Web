7ee1dbf7f0f03fddb8e2a5807454b105cd57da21
JordiPalacios <jordipalacioseso@gmail.com>
2025-04-09 08:14:22 +0200

    texto explicativo para la explicacion 01 añadido

--- src/sections/tatuajes/TatuajesExplicacion01.jsx
+++ src/sections/tatuajes/TatuajesExplicacion01.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
-export const TatuajeExplicacion01 = () => {
+export const TatuajesExplicacion01 = () => {
     return (
-    <div>TatuajeExplicacion01</div>
+    <>
+        <p>
+            Veamos, piensa en la cara de un amigo o un familiar
+            <br />
+            <br />
+            El primero que te venga a la cabeza,
+            <br />
+            <br />
+            <b>¿Lo tienes?</b>
+            <br />
+            <br />
+            Ahora imagínate a TODOS tus invitados <b>con su cara tatuada,</b> en
+            el brazo, en la frente, hasta donde de tu imaginación.
+            <br />
+            <br />
+            <b>Puede ser quien quieras</b> y lo que quieras..
+        </p>
+    </>
     )
 }

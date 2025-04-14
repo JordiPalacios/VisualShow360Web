303c5a54d22888556f56b322508cc7387bb93ae8
JordiPalacios <jordipalacioseso@gmail.com>
2025-04-14 07:35:23 +0200

    tabla añadida con las medidas correspondientes y estilada correctamente

--- src/sections/tatuajes/TatuajesMedidas.jsx
+++ src/sections/tatuajes/TatuajesMedidas.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 export const TatuajesMedidas = () => {
     return (
-        <section>
-        <img src="./img/servicios/tatuajes/tatuaje_02.webp" alt="Tatuaje Personalizado" width="300px" height="auto"/>
+        <section className='tatuajesMedidas'>
+            <div className="tatuajesMedidasExp">
+                <img src="./img/servicios/tatuajes/tatuaje_02.webp" alt="Tatuaje Personalizado" width="300px" height="auto"/>
+                <table className='tableTatuajesMedida' cellPadding="25" cellSpacing="1">
+                    <tbody>                    
+                        <tr>
+                            <td style={{backgroundColor: 'var(--accent)'}}>
+                                <b>150 tatuajes</b>
+                            </td>
+                            <td style={{backgroundColor: 'var(--accent-2)', color: 'darkgreen'}}>
+                                <b>0,50€/unidad</b>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{backgroundColor: 'var(--accent)'}}>
+                                100 tatuajes
+                            </td>
+                            <td style={{backgroundColor: 'var(--accent-2)', color: 'darkgreen'}}>
+                                <b>0,60€/unidad</b>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{backgroundColor: 'var(--accent)'}}>
+                                50 tatuajes
+                            </td>
+                            <td style={{backgroundColor: 'var(--accent-2)', color: 'darkgreen'}}>
+                                <b>0,80€/unidad</b>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             <p>
                 *Para evitar la reventa de tatuajes por parte de otras empresas, el máximo de unidades de compra por web son de 150 unidades.
             </p>
